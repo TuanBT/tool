71d0caef7878e86a26ac12ffd5d0ce3edd5be76b
Tuân Bùi Tiến <bttvn.4t@gmail.com>
2017-05-17 13:31:07 +0700

    Ong troi oi

---
+++ ongtroioi/javascripts/DB.js
@@ -0,0 +1,43 @@
+/**
+ * Created by Bui Tien Tuan on 17/04/2015.
+ */
+db =
+{
+    "FunnyTest": {
+		"Waiting": [
+            {"Text": "Là một ông trời có tâm, câu trả lời của ta đòi hỏi vài giây suy nghĩ...."},
+			{"Text": "Để ta bấm đốt ngón tay tính toán cho ra câu trả lời..."},
+			{"Text": "Nên trả lòi điều gì cho con đây nhỉ..."}
+        ]
+        ,
+        "Normal": [
+            {"Text": "Hãy trả lời ta, con đang gặp vấn đề gì?"},
+			{"Text": "Cho ta biết con muốn gì nào?"},
+			{"Text": "Muốn ta giúp đỡ?, cứ tự nhiên nào!"}
+        ]
+        ,
+        "OneTimes": [
+            {"Text": "Quả thật là một quyết định khó khăn. Chúc con vui với quyết định này. Nếu còn nghi ngờ con có thể hỏi lại lần nữa, nhưng ta khuyến khích con nên đứng dậy và hành động đi đừng hỏi nữa. Con có thể hỏi câu hỏi khác bằng cách F5!"},
+			{"Text": "Đây là câu trả lời của ta. Tiên quyết vẫn nằm trong bàn tay của con. Hãy lắng nghe trái tim mình thật kỹ và hành động con nhé! Con có thể hỏi câu hỏi khác bằng cách nhấn F5!"},
+			{"Text": "Tèn... tén ... ten ... Chúc con vui với quyết định này. Nếu không vui thì có thể hỏi lại lần nữa, nhưng đời thường phải bập bênh lênh đênh. Cứ làm theo những gì trái tim con mách bảo, hỏi 1 lần quá đủ cho một cuộc tình rồi."}
+        ]
+        , "SecondTimes": [
+            {"Text": "Ơ không tin thật à? Hẳn là con đang rơi vào trạng thái bức bí bộc bạch rồi. Hi vọng con sớm đưa ra được quyết định của mình, với sự trợ giúp của ta. Ta lúc nào cũng ở bên khi con cần"},
+            {"Text": "Haiz ... dà ... Con vẫn còn băn khoăn sao... Hi vọng câu trả lời trên đã đủ làm con tin vào quyết định của mình."},
+            {"Text": "Đừng bối rối nữa anh bạn nhỏ. Cứ hành động theo trái tim đi nào. Mong rằng câu trả lời phía trên phần nào củng cố quyết định thật sự trong lòng con."}
+        ]
+        , "ThirdTimes": [
+            {"Text": "Nếu đã hỏi một câu đến lần này thì hẳn trong lòng con đã có sẵn quyết định, có hỏi ta 100 lần nữa thì câu trả lời của bản thân con vẫn là số 1. Hãy làm theo theo trực giác và trái tim con mách bảo. Thôi ta đi đâyyyyyyy! (Nếu hỏi câu khác thì nhớ F5 rồi đọc ba lần cầu thần chú '...' ta sẽ lại hiện lên). Nhưng nếu vẫn câu hỏi cũ thì ... chúc vui!"},
+			{"Text": "Đã là lần ấn thú ba rồi, không lẽ con đang mong đợi điều gì khác nữa?"},
+			{"Text": "Quá tam ba bận, trả lời cho lần này là lần cuối nha!"}
+        ]
+        ,
+        "OverTimes": [
+            {"Text": "Vẫn cứng đầu hả? Thôi, ta trả lời đại đấy, đúng sai không biết đâu nha."},
+			{"Text": "Đúng hay sai ta không biết đâu nha!"},
+			{"Text": "Hỏi gì mà hỏi lắm thế không biết, ta trả lòi như thế đó, tin hay không tùy con."}
+        ]
+    }
+}
+
+
---
+++ ongtroioi/javascripts/js.js
@@ -0,0 +1,169 @@
+﻿var options = [];
+var idChoose;
+var helpTextLine = 4; //1 blank, 2 line, 1 bonus
+var countAsk =0;
+var customOptionValue = ""; //customOptionKey
+
+$(document).ready(function () {
+    //Chọn mặc định nút đầu tiên
+    window.onload = function () {
+        reset();
+        optClick('T1');
+		if(getLocalStorage('customOptionKey')!=null){
+			customOptionValue = getLocalStorage('customOptionKey');
+			document.getElementById('textBoxFirst').value = customOptionValue.split('-')[0];
+			document.getElementById('textBoxSecond').value = customOptionValue.split('-')[1];
+		}else{
+			document.getElementById('textBoxFirst').value = "SẤP";
+			document.getElementById('textBoxSecond').value = "NGỬA";
+		}
+    };
+});//End ready
+
+window.onresize = function (event) {
+    reset();
+};
+
+//Reset
+function reset() {
+    //BoxResult
+    $('#boxClick').css('line-height', $('#boxClick').height() + 12 + "px");
+    //$('#boxClick').css('font-size', Math.floor($('#boxResult').height()*3/4)+"px");
+    $('#boxClick').css('font-size', $('#boxResult').height() + "px");
+    //helpText
+    /*for(var i=1;i<helpTextLine*2;i++){
+     $('#boxResult').css('font-size', Math.floor($('#boxClick').height()/i));
+     if($('#boxResult').height()<=$('#boxClick').height()) break;
+     }*/
+    //helpText
+    for (var i = 1; i < helpTextLine * 2; i++) {
+        $('#helpTextCont').css('font-size', Math.floor($('#helpText').height() / i) + "px");
+        if ($('#helpTextCont').height() <= $('#helpText').height()) break;
+    }
+    //Answer
+    for (var i = 1; i < helpTextLine * 2; i++) {
+        $('#answerContent').css('font-size', Math.floor($('#answer').height() / i) + "px");
+        if ($('#answerContent').height() <= $('#answer').height()) break;
+    }
+    //$('#helpTextCont').css('font-size', Math.floor($('#helpText').height()/4));
+    /*while($('#helpTextCont').height()>=$('#helpText').height()){
+     var fz = $('#helpTextCont').css('font-size');
+     $('#helpTextCont').css('font-size',fz-=1);
+     }*/
+}
+
+//Click một nút chọn kiểu
+function optClick(id) {
+    options = $('#'+id).text().trim().split('-');
+    idChoose = id;
+    var lh = $('#boxClick').height() + 12;
+    //$('#boxClick').html("<div id='boxResult' style='line-height:"+lh+"px'>" + options[0] + "-" + options[1] + "</div>");
+    $('#boxClick').html("<div id='boxResult' style='line-height:" + lh + "px'>HỎI</div>");
+        $('.btnChoose').attr('class', 'btnChoose');
+        $('#' + idChoose).attr('class', 'btnChoose current');
+		$('#answerContent').html(setFunnyText(0));
+    reset();
+}
+
+//Click edit option
+function saveChoose() {
+    var firstText = document.getElementById('textBoxFirst').value;
+    var secondText = document.getElementById('textBoxSecond').value;
+    document.getElementById('T6').innerHTML = firstText + "-" + secondText;
+    $('#editContain').hide();
+    $('#T6').show();
+	setLocalStorage("customOptionKey",firstText + "-" + secondText);
+}
+
+//Quay số kết thúc
+function onComplete() {
+    // $("#answerContent").text("Đó là câu trả lời của ta. Nếu con muốn ấn nữa thì cứ việc, nhưng ta chỉ trả lời thật cho một câu hỏi thôi.");
+    $("#answerContent").text(setFunnyText(countAsk));
+    console.log(countAsk);
+    reset();
+}
+
+//Thực hiện việc quay số
+function doSlot() {
+    $("#boxResult").slotMachine({delay: 450})
+        .shuffle(20, onComplete);
+    countAsk++;
+}
+
+//ChooseOption
+function setChooseOption() {
+    $('#boxClick').html("<div id='boxResult'></div>");
+
+    //Chọn số random
+    if (idChoose == 'T5') {
+        for (var i = 1; i <= 10; i++) {
+            $('#boxResult').append("<div>" + i + "</div>");
+        }
+        return;
+    }
+    //Chọn các trường hợp chữ khác
+    for (var i = 0; i < options.length; i++) {
+        $('#boxResult').append("<div>" + options[i] + "</div>");
+    }
+}
+
+//Ngẫu nhiên funny text
+function setFunnyText(intTypeText) {
+    var arrDb = [];
+    switch (intTypeText) {
+		case -1:
+            arrDb = db.FunnyTest.Waiting;
+            break;
+        case 0:
+            arrDb = db.FunnyTest.Normal;
+            break;
+        case 1:
+            arrDb = db.FunnyTest.OneTimes;
+            break;
+        case 2:
+            arrDb = db.FunnyTest.SecondTimes;
+            break;
+        case 3:
+            arrDb = db.FunnyTest.ThirdTimes;
+            break;
+        default:
+            arrDb = db.FunnyTest.OverTimes;
+            break;
+		
+    }
+    var length = arrDb.length;
+    var ranIndex = Math.floor((Math.random() * length) + 0);
+    return arrDb[ranIndex].Text;
+}
+
+function setLocalStorage(key, value) {
+    if (typeof(Storage) !== "undefined") {
+        localStorage.setItem(key, value);
+    } else {
+        console.log("Browser not support local storage");
+    }
+}
+
+function getLocalStorage(key, value) {
+    if (typeof(Storage) !== "undefined") {
+        return localStorage.getItem(key, value);
+    } else {
+        console.log("Browser not support local storage");
+    }
+}
+
+$(document).ready(function () {
+    //Click box
+    $('#boxClick').click(function () {
+        $("#answerContent").text(setFunnyText(-1));
+        reset();
+        setChooseOption();
+        doSlot();
+    });
+    $('.title').mouseover(function () {
+        //$('.st').hide()
+    });
+    $('.title').mouseout(function () {
+        // $('.st').show( "highlight", 100)
+    });
+});//End ready
