61c8e29b826c18f917c6c3b5191933007517ed96
Tuân Bùi Tiến <bttvn.4t@gmail.com>
2017-05-18 11:58:47 +0700

    update

--- biensoxe/js/script.js
+++ biensoxe/js/script.js
@@ -1,1 +1,297 @@
-function initScreen(){divMap.css({left:divInfoProvince.width(),height:winHeight-20}),divInfoProvince.width()+divMap.width()>=winWidth&&(divMap.css({left:winWidth-divMap.width()}),$("#functionPanel").css({left:10,top:10}),divInfoProvince.css({"margin-left":10,top:winHeight/3}))}function showListProvince(){for(var i=27,e=160,o=0,d=0;d<codeProvince.length;d++)for(var c=0;c<codeProvince[d].code.length;c++)o++;var n=Math.round(winHeight/i),l=$("#listProvince1");l.css({left:divInfoProvince.width()+divMap.width()});for(var t="<li class='var_nav {{proId}}' id='li-{{proCode}}'><div class='link_bg'></div><div class='link_title'><div class=icon><i>{{proCode}}</i></div><a onclick='clickProvince(\"{{proId}}\")' onmouseout='darkProvince(\"{{proId}}\")' onmouseover='lightProvince(\"{{proId}}\")'><span class='provinceName'>{{proTitle}}</span></a></div></li>",r=0,s=1,d=0;d<codeProvince.length;d++)for(var c=0;c<codeProvince[d].code.length;c++){r>n&&(r=0,s++,divNavListProvice.append("<ul class='listProvince' id='listProvince"+s+"'></ul>"),$("#listProvince"+s).css({left:divInfoProvince.width()+divMap.width()+(s-1)*e})),r++;var a=t.replace("{{proCode}}",codeProvince[d].code[c]),a=a.replace("{{proCode}}",codeProvince[d].code[c]),a=a.replace("{{proTitle}}",codeProvince[d].title),a=a.replace("{{proId}}",codeProvince[d].id),a=a.replace("{{proId}}",codeProvince[d].id),a=a.replace("{{proId}}",codeProvince[d].id),a=a.replace("{{proId}}",codeProvince[d].id);$("#listProvince"+s).append(a)}divNavListProvice.css({top:(winHeight-l.height())/2}),divInfoProvince.width()+divMap.width()+s*e>=winWidth&&(divNavListProvice.hide(),divMap.css({opacity:.7}))}function showDetailInfo(i){resetInfoDetailDiv(),$("#infoProvinceTitle").html(i.title);for(var e=0;e<i.code.length;e++)$("#listNumberContain").append("<li>"+i.code[e]+"</li>");for(var e=0;e<i.area.length;e++)$("#detailInfoContain").append("<li><div class='detailInfoCode'>"+i.area[e].code+"</div> <div class='detailInfoTitle'>"+i.area[e].title+"</div></li>")}function clickProvince(i){if(idClicked==i)return idClicked="",$("."+i).removeClass("activeClick"),void $("#"+i).css({fill:mapOverColor});idClicked!=i&&""!=idClicked&&($("."+idClicked).removeClass("activeClick"),$("#"+idClicked).css({fill:mapColor})),idClicked=i,$("."+i).addClass("activeClick"),$("#"+i).css({fill:mapClickColor});for(var e=0;e<codeProvince.length;e++)if(codeProvince[e].id==i)return void showDetailInfo(codeProvince[e])}function lightProvince(i){idClicked==i&&$("."+i).removeClass("activeClick"),$("#"+codeProvince[index].id).css({fill:mapColor}),$("#li-"+codeProvince[index].code[indexCode]).removeClass("active"),$("#"+i).css({fill:mapOverColor});for(var e=0;e<codeProvince.length;e++)if(codeProvince[e].id==i)return void showDetailInfo(codeProvince[e])}function darkProvince(i){if(idClicked!=i?$("#"+i).css({fill:mapColor}):($("."+i).addClass("activeClick"),$("#"+i).css({fill:mapClickColor})),resetInfoDetailDiv(),""!=idClicked)for(var e=0;e<codeProvince.length;e++)if(codeProvince[e].id==idClicked)return void showDetailInfo(codeProvince[e])}function showProvince(){index==codeProvince.length-1&&(index=0),$("#li-"+codeProvince[index].code[indexCode]).addClass("active"),$("#"+codeProvince[index].id).css({fill:mapOverColor}),showDetailInfo(codeProvince[index]),timeout=setTimeout(function(){$("#"+codeProvince[index].id).css({fill:mapColor}),$("#li-"+codeProvince[index].code[indexCode]).removeClass("active"),lenghtCode=codeProvince[index].code.length,indexCode++,indexCode>=lenghtCode&&(index++,indexCode=0),showProvince()},500)}function stopTimeout(){clearTimeout(timeout)}function showCode(){100==codeNum&&(codeNum=10),codeNum++;for(var i=0;i<codeProvince.length;i++)for(var e=0;e<codeProvince[i].code.length;e++)if(codeProvince[i].code[e]==codeNum){$("#"+codeProvince[i].id).css({fill:mapOverColor}),$(".content").html(codeProvince[i].code[e]+"-"+codeProvince[i].title),idNum=i;break}setTimeout(function(){$("#"+codeProvince[idNum].id).css({fill:mapColor}),$(".content").html(codeNum),showCode()},200)}function resetInfoDetailDiv(){$("#listNumberContain").empty(),$("#detailInfoContain").empty(),$("#infoProvinceTitle").html("")}var mapColor="#f1c40f",mapOverColor="#16a085",bacgroundColor="#ff6862",mapClickColor="#c0392b",idClicked="",winHeight=$(window).height(),winWidth=$(window).width();$(document).ready(function(){divMap=$("#mapArea"),divInfoProvince=$("#infoProvince"),divNavListProvice=$("#navListProvince"),initScreen(),showListProvince(),$(".land").mouseover(function(){$("#"+codeProvince[index].id).css({fill:mapColor}),$("#li-"+codeProvince[index].code[indexCode]).removeClass("active"),""!=idClicked&&$("."+idClicked).removeClass("activeClick"),idClicked!=this.id&&""!=idClicked&&$("."+idClicked).addClass("activeClick"),$("#"+this.id).css({fill:mapOverColor}),$("."+this.id).addClass("active");for(var i=0;i<codeProvince.length;i++)if(codeProvince[i].id==this.id)return void showDetailInfo(codeProvince[i])}),$(".land").mouseout(function(){if(idClicked!=this.id?$("#"+this.id).css({fill:mapColor}):idClicked==this.id&&$("#"+this.id).css({fill:mapClickColor}),""!=idClicked&&$("."+idClicked).addClass("activeClick"),$("."+this.id).removeClass("active"),resetInfoDetailDiv(),""!=idClicked)for(var i=0;i<codeProvince.length;i++)if(codeProvince[i].id==idClicked)return void showDetailInfo(codeProvince[i])}),$(".land").click(function(){if(idClicked==this.id)return idClicked="",$("."+this.id).removeClass("activeClick"),void $("#"+this.id).css({fill:mapOverColor});idClicked!=this.id&&""!=idClicked&&($("."+idClicked).removeClass("activeClick"),$("#"+idClicked).css({fill:mapColor})),idClicked=this.id,$("."+this.id).addClass("activeClick"),$("#"+this.id).css({fill:mapClickColor});for(var i=0;i<codeProvince.length;i++)if(codeProvince[i].id==this.id)return void showDetailInfo(codeProvince[i])}),$("#funcPlayPause").on("click",".buttonPlayPause",function(){$(this).hasClass("play")?($(this).removeClass("play").addClass("stop"),document.getElementById("ani-left-to-stop").beginElement(),document.getElementById("ani-right-to-stop").beginElement(),showProvince()):$(this).hasClass("stop")&&($(this).removeClass("stop").addClass("play"),document.getElementById("ani-left-to-play").beginElement(),document.getElementById("ani-right-to-play").beginElement(),stopTimeout())}),$("#mainGround").click(function(){""!=idClicked&&($("."+idClicked).removeClass("activeClick"),$("#"+idClicked).css({fill:mapColor}),idClicked="")})});var index=0,indexCode=0,lenghtCode=0,timeout,codeNum=10,idNum=0;
+var mapColor = "#f1c40f"; //Vàng nhạt
+var mapOverColor = "#16a085"; //Xanh lam nhạt
+var bacgroundColor = "#ff6862"; //Dỏ nhạt
+var mapClickColor = "#c0392b"; //Đỏ đậm
+var idClicked = "";
+var winHeight = $(window).height();
+var winWidth = $(window).width();
+
+
+$(document).ready(function () {
+    divMap = $("#mapArea");
+    divInfoProvince = $("#infoProvince");
+    divNavListProvice = $("#navListProvince");
+
+    initScreen();
+    showListProvince();
+
+    //Mouse over map
+    $(".land").mouseover(function () {
+        $("#" + codeProvince[index].id).css({ fill: mapColor });
+        $("#li-" + codeProvince[index].code[indexCode]).removeClass('active');
+        if (idClicked != "") {
+            $("." + idClicked).removeClass('activeClick');
+        }
+        if (idClicked != this.id && idClicked != "") {
+            $("." + idClicked).addClass('activeClick');
+        }
+
+        $("#" + this.id).css({ fill: mapOverColor });
+        $("." + this.id).addClass('active');
+
+        for (var i = 0; i < codeProvince.length; i++) {
+            if (codeProvince[i].id == this.id) {
+                showDetailInfo(codeProvince[i]);
+                return;
+            }
+        }
+    })
+
+    //Mouse over out
+    $(".land").mouseout(function () {
+        if (idClicked != this.id) {
+            $("#" + this.id).css({ fill: mapColor });
+        } else if (idClicked == this.id) {
+            $("#" + this.id).css({ fill: mapClickColor });
+        }
+        if (idClicked != "") {
+            $("." + idClicked).addClass('activeClick');
+        }
+        $("." + this.id).removeClass('active');
+
+        resetInfoDetailDiv();
+
+        if (idClicked != "") {
+            for (var i = 0; i < codeProvince.length; i++) {
+                if (codeProvince[i].id == idClicked) {
+                    showDetailInfo(codeProvince[i]);
+                    return;
+                }
+            }
+        }
+    })
+
+    $(".land").click(function () {
+        if (idClicked == this.id) {
+            idClicked = "";
+            $("." + this.id).removeClass('activeClick');
+            $("#" + this.id).css({ fill: mapOverColor });
+            return;
+        }
+        if (idClicked != this.id && idClicked != "") {
+            $("." + idClicked).removeClass('activeClick');
+            $("#" + idClicked).css({ fill: mapColor });
+        }
+        idClicked = this.id;
+        $("." + this.id).addClass('activeClick');
+        $("#" + this.id).css({ fill: mapClickColor });
+        for (var i = 0; i < codeProvince.length; i++) {
+            if (codeProvince[i].id == this.id) {
+                showDetailInfo(codeProvince[i]);
+                return;
+            }
+        }
+    });
+
+    //Play pause map
+    $('#funcPlayPause').on('click', '.buttonPlayPause', function () {
+        if ($(this).hasClass('play')) {
+            $(this).removeClass('play').addClass('stop');
+            document.getElementById('ani-left-to-stop').beginElement();
+            document.getElementById('ani-right-to-stop').beginElement();
+            showProvince();
+        } else
+            if ($(this).hasClass('stop')) {
+                $(this).removeClass('stop').addClass('play');
+                document.getElementById('ani-left-to-play').beginElement();
+                document.getElementById('ani-right-to-play').beginElement();
+                stopTimeout();
+            }
+    });
+
+    $("#mainGround").click(function () {
+        if (idClicked != "") {
+            $("." + idClicked).removeClass('activeClick');
+            $("#" + idClicked).css({ fill: mapColor });
+            idClicked = "";
+        }
+    });
+});
+
+function initScreen() {
+    divMap.css({ left: divInfoProvince.width(), height: winHeight - 20 });
+
+    if (divInfoProvince.width() + divMap.width() >= winWidth) {
+        divMap.css({ left: winWidth - divMap.width() });
+        $("#functionPanel").css({ left: 10, top: 10 });
+        divInfoProvince.css({ "margin-left": 10, top: winHeight / 3 });
+    }
+}
+
+function showListProvince() {
+    var heightLi = 27;
+    var widthLi = 160;
+    var numCode = 0;
+    for (var i = 0; i < codeProvince.length; i++) {
+        for (var j = 0; j < codeProvince[i].code.length; j++) {
+            numCode++;
+        }
+    }
+
+    var numLine = Math.round(winHeight / heightLi);
+
+    var divListPro1 = $("#listProvince1");
+    divListPro1.css({ "left": divInfoProvince.width() + divMap.width() });
+
+    var divLi = "<li class='var_nav {{proId}}' id='li-{{proCode}}'><div class='link_bg'></div><div class='link_title'><div class=icon><i>{{proCode}}</i></div>"
+        + "<a onclick='clickProvince(\"{{proId}}\")' onmouseout='darkProvince(\"{{proId}}\")' onmouseover='lightProvince(\"{{proId}}\")'><span class='provinceName'>{{proTitle}}</span></a></div></li>";
+
+    var countNumLine = 0;
+    var countNumRow = 1;
+    for (var i = 0; i < codeProvince.length; i++) {
+        for (var j = 0; j < codeProvince[i].code.length; j++) {
+            if (countNumLine > numLine) {
+                countNumLine = 0;
+                countNumRow++;
+                divNavListProvice.append("<ul class='listProvince' id='listProvince" + countNumRow + "'></ul>");
+                $("#listProvince" + countNumRow).css({ "left": divInfoProvince.width() + divMap.width() + (countNumRow - 1) * widthLi });
+            }
+            countNumLine++;
+            var res = divLi.replace("{{proCode}}", codeProvince[i].code[j]);
+            var res = res.replace("{{proCode}}", codeProvince[i].code[j]);
+            var res = res.replace("{{proTitle}}", codeProvince[i].title);
+            var res = res.replace("{{proId}}", codeProvince[i].id);
+            var res = res.replace("{{proId}}", codeProvince[i].id);
+            var res = res.replace("{{proId}}", codeProvince[i].id);
+            var res = res.replace("{{proId}}", codeProvince[i].id);
+            $('#listProvince' + countNumRow).append(res);
+        }
+    }
+    divNavListProvice.css({ "top": (winHeight - divListPro1.height()) / 2 });
+    if (divInfoProvince.width() + divMap.width() + countNumRow * widthLi >= winWidth) {
+        //divNavListProvice.hide();
+        divMap.css({ opacity: 0.7 });
+    }else{
+
+    }
+}
+
+function showDetailInfo(codeProvinceOne) {
+    resetInfoDetailDiv();
+
+    $("#infoProvinceTitle").html(codeProvinceOne.title);
+    for (var i = 0; i < codeProvinceOne.code.length; i++) {
+        $("#listNumberContain").append("<li>" + codeProvinceOne.code[i] + "</li>");
+    }
+
+    for (var i = 0; i < codeProvinceOne.area.length; i++) {
+        $("#detailInfoContain").append("<li><div class='detailInfoCode'>" + codeProvinceOne.area[i].code + "</div> <div class='detailInfoTitle'>" + codeProvinceOne.area[i].title + "</div></li>");
+    }
+}
+
+function clickProvince(provinceId) {
+    if (idClicked == provinceId) {
+        idClicked = "";
+        $("." + provinceId).removeClass('activeClick');
+        $("#" + provinceId).css({ fill: mapOverColor });
+        return;
+    }
+    if (idClicked != provinceId && idClicked != "") {
+        $("." + idClicked).removeClass('activeClick');
+        $("#" + idClicked).css({ fill: mapColor });
+    }
+    idClicked = provinceId;
+    $("." + provinceId).addClass('activeClick');
+    $("#" + provinceId).css({ fill: mapClickColor });
+    for (var i = 0; i < codeProvince.length; i++) {
+        if (codeProvince[i].id == provinceId) {
+            showDetailInfo(codeProvince[i]);
+            return;
+        }
+    }
+};
+
+function lightProvince(provinceId) {
+    if (idClicked == provinceId) {
+        $("." + provinceId).removeClass('activeClick');
+    }
+    $("#" + codeProvince[index].id).css({ fill: mapColor });
+    $("#li-" + codeProvince[index].code[indexCode]).removeClass('active');
+
+    $("#" + provinceId).css({ fill: mapOverColor });
+    for (var i = 0; i < codeProvince.length; i++) {
+        if (codeProvince[i].id == provinceId) {
+            showDetailInfo(codeProvince[i]);
+            return;
+        }
+    }
+}
+function darkProvince(provinceId) {
+    if (idClicked != provinceId) {
+        $("#" + provinceId).css({ fill: mapColor });
+    } else {
+        $("." + provinceId).addClass('activeClick');
+        $("#" + provinceId).css({ fill: mapClickColor });
+    }
+
+    resetInfoDetailDiv();
+
+    if (idClicked != "") {
+        for (var i = 0; i < codeProvince.length; i++) {
+            if (codeProvince[i].id == idClicked) {
+                showDetailInfo(codeProvince[i]);
+                return;
+            }
+        }
+    }
+}
+
+var index = 0;
+var indexCode = 0;
+var lenghtCode = 0;
+var timeout;
+function showProvince() {
+    if (index == codeProvince.length - 1) {
+        index = 0;
+    }
+
+    $("#li-" + codeProvince[index].code[indexCode]).addClass('active');
+    $("#" + codeProvince[index].id).css({ fill: mapOverColor });
+    showDetailInfo(codeProvince[index]);
+    timeout = setTimeout(function () {
+        $("#" + codeProvince[index].id).css({ fill: mapColor });
+        $("#li-" + codeProvince[index].code[indexCode]).removeClass('active');
+
+        lenghtCode = codeProvince[index].code.length;
+        indexCode++;
+        if (indexCode >= lenghtCode) {
+            index++;
+            indexCode = 0;
+        }
+        showProvince();
+    }, 500);
+}
+
+function stopTimeout() {
+    clearTimeout(timeout);
+}
+
+var codeNum = 10;
+var idNum = 0;
+function showCode() {
+    if (codeNum == 100) {
+        codeNum = 10;
+    }
+    codeNum++;
+    for (var k = 0; k < codeProvince.length; k++) {
+        for (var j = 0; j < codeProvince[k].code.length; j++) {
+            if (codeProvince[k].code[j] == codeNum) {
+                $("#" + codeProvince[k].id).css({ fill: mapOverColor });
+                $(".content").html(codeProvince[k].code[j] + "-" + codeProvince[k].title);
+                idNum = k;
+                break;
+            }
+        }
+    }
+    setTimeout(function () {
+        $("#" + codeProvince[idNum].id).css({ fill: mapColor });
+        $(".content").html(codeNum);
+        showCode();
+    }, 200);
+}
+
+function resetInfoDetailDiv() {
+    $("#listNumberContain").empty();
+    $("#detailInfoContain").empty();
+    $("#infoProvinceTitle").html("");
+}
