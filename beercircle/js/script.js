ec7b89548296455ddaf150df10821a3057d4f4a0
tuan <bttvn.4t@gmail.com>
2018-07-07 23:49:45 +0700

    a

--- beercircle/js/script.js
+++ beercircle/js/script.js
@@ -1,7 +1,22 @@
 $(document).ready(function () {
+    var canvas = document.getElementById('canvas');
+    
+    var docWi = $(document).width();
+    var docHe = $(document).height();
 
+    var minDoc = docWi > docHe ? docHe : docWi;
+
+    canvas.style.width  = minDoc + 'px';
+    canvas.style.height = minDoc + 'px';
 });//End ready
 
 
 window.onresize = function (event) {
+    docWi = $(document).width();
+    docHe = $(document).height();
+
+    minDoc = docWi > docHe ? docHe : docWi;
+
+    canvas.style.width  = minDoc + 'px';
+    canvas.style.height = minDoc + 'px';
 };
